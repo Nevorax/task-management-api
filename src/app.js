5efd4b163d437b12c14835ab4540e82e1a251a11
Sebanev15 <sneves@impulso.edu.uy>
2026-03-19 19:54:25 -0300

    add initial structure of the proyect with his respectives dependencies. Also create a basic routes of '/' and '/tasks' with a JSON respond

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API funcionando 🚀');
+});
+
+const taskRoutes = require('./routes/tasks.routes');
+
+app.use('/tasks', taskRoutes);
+
+
+module.exports = app;
