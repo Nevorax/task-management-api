bfa54df88cfe0c310425f8518b748c467d76eb88
Sebanev15 <sneves@impulso.edu.uy>
2026-03-25 19:32:05 -0300

    set up the base Prisma + PostgreSQL configuration and update project ignore rules

---
+++ src/config/prisma.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('../generated/prisma');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error('DATABASE_URL is not defined in environment variables');
+}
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+module.exports = prisma;
